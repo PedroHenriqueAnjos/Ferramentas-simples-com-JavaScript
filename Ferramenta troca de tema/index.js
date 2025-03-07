600b019b98bd80ed3c82da057231cc84ff9c5b48
Pedro Henrique dos Anjos <pedrohenriqueoliveiradosanjos@gmail.com>
2025-03-06 21:38:53 -0300

    rslt

--- Ferramenta troca de tema/index.js
+++ Ferramenta troca de tema/index.js
@@ -1,5 +1,5 @@
 function alterar() {
-    document.getElementById('paragrafo').style.color = 'white';
+    
     document.getElementById('foto').src = 'fotos.img/lua.png';
     document.body.style.backgroundColor = '	#363636';
    document.getElementById('14').style.backgroundColor = '#363636'
