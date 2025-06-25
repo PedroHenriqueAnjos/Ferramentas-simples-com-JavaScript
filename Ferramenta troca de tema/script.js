8a7d672af74395139618cbed34a2ebe2bc32d0a4
phanjoszxx <pedrohenriqueoliveiradosanjos@gmail.com>
2025-06-25 16:21:59 -0300

    Delete Ferramenta troca de tema directory

--- Ferramenta troca de tema/script.js
+++
@@ -1,7 +0,0 @@
-function alterar() {
-    document.getElementById('paragrafo').style.color = 'white';
-    document.getElementById('foto').src = 'fotos.img/lua.png';
-    document.body.style.backgroundColor = '	#363636';
-   document.getElementById('14').style.backgroundColor = '#363636'
-
-}
